d92af7cb4ec330d837e4e518a044abfd40938635
shivangik11 <89596201+shivangik11@users.noreply.github.com>
2024-07-03 21:57:48 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Analytics />
       </Routes>
     </Router>
   );
